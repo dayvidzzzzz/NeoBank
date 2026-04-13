7783ecbe9b9401aee93c7c6a2c52beb9d988e498
dayvidzzzzz <aldayvid@gmail.com>
2026-04-13 16:38:25 -0300

    first commit

---
+++ src/main/java/dy/system/minebanconovo/MineBancoNovoApplication.java
@@ -0,0 +1,13 @@
+package dy.system.minebanconovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MineBancoNovoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MineBancoNovoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dy/system/minebanconovo/MineBancoNovoApplicationTests.java
@@ -0,0 +1,13 @@
+package dy.system.minebanconovo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MineBancoNovoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
